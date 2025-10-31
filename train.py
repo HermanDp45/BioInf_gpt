ae189eaa04c5bbd3fa2fce538d0dde7dace0ce24
HermannDp45 <HermannDp@edelkin.ru>
2025-10-31 13:41:09 +0300

    update meta process in train

--- train.py
+++ train.py
@@ -179,6 +179,13 @@ with open(test_sequences_path, 'rb') as f:
 with open(meta_path, 'rb') as f:
     meta = pickle.load(f)
 
+if data_type == 'init_seq':
+    chars = [c for c in meta['itos'].values() if c != '-']
+    meta['itos'] = {i: ch for i, ch in enumerate(chars)}
+    meta['stoi'] = {ch: i for i, ch in enumerate(chars)}
+    meta['vocab_size'] = len(chars)
+    print(f"Updated vocab_size to {meta['vocab_size']} (removed '-')")
+
 meta_vocab_size = meta['vocab_size']
 print(f"found vocab_size = {meta_vocab_size} (inside {meta_path})")
 
@@ -203,26 +210,6 @@ val_loader = DataLoader(
 # =====================
 #   End new block
 # =====================
-
-# # poor man's data loader
-# data_dir = os.path.join('data', dataset)
-# def get_batch(split):
-#     # We recreate np.memmap every batch to avoid a memory leak, as per
-#     # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
-#     if split == 'train':
-#         data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
-#     else:
-#         data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
-#     ix = torch.randint(len(data) - block_size, (batch_size,))
-#     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
-#     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
-#     if device_type == 'cuda':
-#         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-#         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
-#     else:
-#         x, y = x.to(device), y.to(device)
-#     return x, y
-
 
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
